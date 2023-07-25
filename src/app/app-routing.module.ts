16a3bebbf48aebcfa50d396ce40be65718da0f2a
Mysteryeditor <sri027.cvb@gmail.com>
2023-07-25 16:48:22 +0530

    routing module commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostComponent } from './post/post.component';
 import { HelpsComponent } from './helps/helps.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 const routes: Routes = [
-  {path:"post", component:PostComponent},
+  { path: 'post', component: PostComponent },
   {
-    path:"help",component:HelpsComponent
+    path: 'help',
+    component: HelpsComponent,
+  },
+  {
+    path:'signup',
+    component:SignUpComponent
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
