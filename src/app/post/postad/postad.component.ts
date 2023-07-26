3f2b51612ed95eecece32a54cd5b3d7fbac7bbe5
Amigosouls <amigosouls022@gmail.com>
2023-07-26 17:49:29 +0530

    postad unfinished commit

--- src/app/post/postad/postad.component.ts
+++ src/app/post/postad/postad.component.ts
@@ -18,17 +18,32 @@ export class PostadComponent implements OnInit {
   selectedNodes: any;
   nodes: any[]=[
     {
-      key:0,
-      label:"TVS",
-      children:[
+      key: '0',
+    label: 'Documents',
+    data: 'Documents Folder',
+    icon: 'pi pi-fw pi-inbox',
+    children: [
         {
-          key:0-0,
-          label:"Apache"
+            key: '0-0',
+            label: 'Work',
+            data: 'Work Folder',
+            icon: 'pi pi-fw pi-cog',
+            children: [
+                { key: '0-0-0', label: 'Expenses.doc', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
+                { key: '0-0-1', label: 'Resume.doc', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
+            ]
+        },
+        {
+            key: '0-1',
+            label: 'Home',
+            data: 'Home Folder',
+            icon: 'pi pi-fw pi-home',
+            children: [{ key: '0-1-0', label: 'Invoices.txt', icon: 'pi pi-fw pi-file', data: 'Invoices for this month' }]
         }
-      ]
+    ]
     }
   ];
- // bikeList:Array<string>=["TVS","Bajaj","Hero","YAMAHA","KTM","RE","HONDA",""]
+ bikeList:Array<any>=[]
   postAdForm!: FormGroup;
   firstFormGroup!: FormGroup;
   secondFormGroup!: FormGroup;
@@ -51,4 +66,12 @@ export class PostadComponent implements OnInit {
     })
   
   }
+  getImage(vechicle:string){
+    fetch(`https://pixabay.com/api/?key=36007746-b36ae27c3528436e0e7b2219a&q=${q}&image_type=photo&category=${category}&min_width=300&min_height=400&order=${order}`,{method:"GET"  //api link from pixaby
+    }).then(res => res.json())
+    .then(image=>{
+        
+    
+    })
+}
 }
