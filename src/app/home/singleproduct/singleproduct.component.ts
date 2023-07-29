407ec61c4c58cf7ed13bbf8e772903d7ba758387
Mysteryeditor <sri027.cvb@gmail.com>
2023-07-29 16:16:56 +0530

    favorites end-commit

--- src/app/home/singleproduct/singleproduct.component.ts
+++ src/app/home/singleproduct/singleproduct.component.ts
@@ -1,19 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Favourites } from 'src/models/favourites';
+import { PostAd } from 'src/models/postad';
 import { FavouritesService } from 'src/services/favourites.service';
+import { RegisterationService } from 'src/services/registeration.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-singleproduct',
   templateUrl: './singleproduct.component.html',
-  styleUrls: ['./singleproduct.component.css']
+  styleUrls: ['./singleproduct.component.css'],
 })
-export class SingleproductComponent {
-  constructor(private router: Router, private favService: FavouritesService) { }
+export class SingleproductComponent implements OnInit {
+  activeUserId: number = 0;
+  constructor(private router: Router, private favService: FavouritesService,private registerService:RegisterationService) {
+   
+  }
+
+  ngOnInit(): void {
+   
+  }
+
+
+
+
   @Input() products: any;
   public show: boolean = true;
   toggle() {
-    return this.show = !this.show;
+    return (this.show = !this.show);
   }
   productId!: number;
 
@@ -21,6 +35,8 @@ export class SingleproductComponent {
     this.productId = id;
     return this.router.navigate(['viewProduct/' + id]);
   }
+
+
 
   fav: Favourites = {
     brand: '',
@@ -31,11 +47,16 @@ export class SingleproductComponent {
     price: 0,
     city: '',
     imagelink: '',
-    user_id: 0
-  }
-  
-  addtoFav(products: any) {
-    this.fav.brand = products.brand;
+    user_id: 0,
+  };
+
+  addtoFav(products: Favourites) {
+    
+
+    this.registerService.getActiveUser().subscribe(
+      (response)=>
+      {
+                this.fav.brand = products.brand;
     this.fav.year = products.year;
     this.fav.kmdriven = products.kmdriven;
     this.fav.adtitle = products.adtitle;
@@ -43,7 +64,22 @@ export class SingleproductComponent {
     this.fav.price = products.price;
     this.fav.city = products.city;
     this.fav.imagelink = products.imagelink;
-    this.fav.user_id = products.user_id;
-    this.favService.addtoFav(this.fav);
+    this.fav.user_id = response[0].id;
+        console.log(this.activeUserId);
+        const token = localStorage.getItem('token');
+        if (token) {
+          this.favService.addtoFav(this.fav);
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Please login to continue',
+            text: 'You need an account for adding favourite list',
+          });
+          this.router.navigate(['/signin']);
+        }
+      }
+    )
+  
+   
   }
 }
