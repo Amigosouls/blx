c28b69e626351d303999adad183536a3f393baca
Vignesh3223 <viketvicky@gmail.com>
2023-07-29 16:42:38 +0530

    favourites

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Favourites } from 'src/models/favourites';
 import { FavouritesService } from 'src/services/favourites.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-favourites',
@@ -26,6 +27,25 @@ export class FavouritesComponent implements OnInit {
   favData: any = [];
 
   favourites: Favourites[] = [];
+
+
+  removeFav(deleteItem: Favourites) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "Want to remove",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.favService.removeFavorite(deleteItem);
+        setTimeout(() => { this.ngOnInit() }, 500);
+      }
+    });
+
+  }
 
   ngOnInit(): void {
     this.favService.getFavourites().subscribe(
