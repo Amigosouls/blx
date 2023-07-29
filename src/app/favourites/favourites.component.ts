c09bd9299af359439014432b340673c16260592d
Vignesh3223 <viketvicky@gmail.com>
2023-07-29 11:59:27 +0530

    recent in favourites

--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Favourites } from 'src/models/favourites';
+import { FavouritesService } from 'src/services/favourites.service';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.component.html',
   styleUrls: ['./favourites.component.css']
 })
-export class FavouritesComponent {
+export class FavouritesComponent implements OnInit {
 
+  constructor(private favService: FavouritesService) { }
+
+  fav: Favourites = {
+    brand: '',
+    year: 0,
+    kmdriven: 0,
+    adtitle: '',
+    description: '',
+    price: 0,
+    city: '',
+    imagelink: '',
+    user_id: 0
+  }
+
+  favData: any = [];
+
+  favourites: Favourites[] = [];
+
+  ngOnInit(): void {
+    this.favService.getFavourites().subscribe(
+      (res) => {
+        this.favourites = res;
+        console.log(this.favourites);
+      });
+  }
 }
